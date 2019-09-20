691cc546a0dd11dd2757a5075d18f177fa9108c9
PrakharRai07 <53911572+PrakharRai07@users.noreply.github.com>
2019-09-20 21:42:36 +0530

    LED chaser
    Ardunio code for chaser of LED

---
+++ programs/chaser.ino
@@ -0,0 +1,17 @@
+
+void setup() {
+
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+void loop() 
+{    int i;
+  for(i=10;i<=13;i++)
+  {
+   digitalWrite(i, HIGH);  
+  delay(1000);                     
+   digitalWrite(i, LOW);    
+  }                      
+}
